e12af63784d957ced4d7f7c3cea7be205bd1fa89
Tahj Dosso <tahjdosso@10.60.7.240>
2019-03-20 14:10:38 -0400

    Since we need to do abstraction, I created a faculty member class. Where a teacher is a subclass of.

---
+++ src/test/java/FacultyMember.java
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+// File:             FacultyMember.java
+// Course:           CSC232B, Spring 2019
+// Authors:          (your name and the names of other members of your group)
+//
+// Acknowledgements: (list anyone else other than your instructor who helped)
+//                   (describe in detail the the ideas and help they provided)
+//
+// Online sources:   (include Web URLs and description of any information used)
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * 
+ */
+public abstract class FacultyMember {
+   /*
+
+    */
+       private String name;// name for faculty
+       private int age; // age for faculty
+       private String department; // department for faculty
+
+       public abstract String getName(); // gets name
+       public abstract int getAge(); //gets name
+       public abstract String getDepartment(); // gets department 
+
+}
